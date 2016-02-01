f2d5bdebfdeb54f4deb98759b118b6855d9a38f6
unknown <skb12156@CIS-ARROL-38.cis.strath.ac.uk>
2016-02-01 10:53:59 +0000

    Added text view and attempting to add listener for each button, should make uml for over all layout as soon as I grasp what the structure needs to be like

--- MooSeum/app/src/main/java/baldeeps/mooseum/MainMenu.java
+++ MooSeum/app/src/main/java/baldeeps/mooseum/MainMenu.java
@@ -2,6 +2,8 @@ package baldeeps.mooseum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        final TextView textbox = (TextView) findViewById(R.id.textView);
+        Button play_button = (Button) findViewById(R.id.play_button);
+        Button connect_button = (Button) findViewById(R.id.connect_button);
+        Button scan_button = (Button) findViewById(R.id.scan_button);
+
+        play_button.setOnClickListener(new PlayButtonListener());
     }
 }
---
+++ MooSeum/app/src/main/java/baldeeps/mooseum/PlayButtonListener.java
@@ -0,0 +1,11 @@
+package baldeeps.mooseum;
+
+import android.view.View;
+
+/**
+ * Created by skb12156 on 01/02/2016.
+ */
+public class PlayButtonListener extends View{
+
+
+}
