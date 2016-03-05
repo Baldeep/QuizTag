70bdd546ec42b15d1d903fe69eb27bf787874c6d
Baldeep <baldeepsingh.93@gmail.com>
2016-03-05 15:57:11 +0000

    restarted by following the fermentos turorial more closely, however scanning a tag that has been written still opens up the text viewer and doesn't get detected when hitting back and getting onto the app again, problem is most likely in the intent handling

--- QuizTagApp/app/src/main/java/baldeep/quiztagapp/Frontend/NFC_Tag_Writer.java
+++ QuizTagApp/app/src/main/java/baldeep/quiztagapp/Frontend/NFC_Tag_Writer.java
@@ -11,6 +11,7 @@ import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,6 +19,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,118 +32,132 @@ import baldeep.quiztagapp.R;
  */
 public class NFC_Tag_Writer extends AppCompatActivity {
 
-    private IntentFilter[] intentFilters;
-    private PendingIntent pendingIntent;
-    private Tag nfcTag;
+    private TextView textView;
+    private EditText editText;
 
-    private TextView tv;
+
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    IntentFilter intentFileters[];
+    Tag tag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.nfc_writer);
 
-        tv = (TextView) findViewById(R.id.nfc_text_view);
+        textView = (TextView) findViewById(R.id.nfc_text_view);
+        editText = (EditText) findViewById(R.id.nfc_text);
 
-        Button btn = (Button) findViewById(R.id.nfc_button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button read_btn = (Button) findViewById(R.id.nfc_read_button);
+        Button write_btn = (Button) findViewById(R.id.nfc_write_button);
+
+        // Check the adapter
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if(nfcAdapter == null || !nfcAdapter.isEnabled()){
+            DialogFragment df = new NFCInfoDialog();
+            Bundle nfcBundle = new Bundle();
+            nfcBundle.putString("title", "NFC Hardware");
+            nfcBundle.putString("message", "Check NFC is enabled");
+            nfcBundle.putString("type", "nfcOff");
+            df.setArguments(nfcBundle);
+            df.show(getFragmentManager(), "NFC Info");
+        }
+
+        // Set an intent filter
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter intentFilter = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+        intentFileters = new IntentFilter[]{intentFilter};
+
+        // Just put this here for now for the write button
+        // based on tutorial:
+        // http://www.framentos.com/en/android-tutorial/2012/07/31/write-hello-world-into-a-nfc-tag-with-a/
+        write_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(NFCHardwareAvailable() && nfcTag!= null && NFCTagIsValid()){
-                    readTag(getIntent());
-                }
+                    if(tag != null){
+                        // Take the tag no matter the format
+                        writeTag(editText.getText().toString(), tag);
+                    } else {
+                        Toast.makeText(NFC_Tag_Writer.this, "No Tag Detected", Toast.LENGTH_SHORT).show();
+                    }
             }
         });
-
-
-        pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0,
-                new Intent(this, getClass()).
-                        addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-        IntentFilter NdefIntent = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
-        intentFilters = new IntentFilter[]{NdefIntent};
-
     }
 
-    private boolean NFCTagIsValid(){
-        Ndef ndef = Ndef.get(nfcTag);
-        if(ndef == null){
-            Toast.makeText(this, "Tag is not valid", Toast.LENGTH_SHORT).show();
-            return false;
+    /**
+     * As this deals with NFC tags, the OnNewIntent method needs to be overridden in order to be
+     * able to get the new tag.
+     * @param intent
+     */
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if(nfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
+            tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         }
-
-        NdefMessage message = ndef.getCachedNdefMessage();
-        if(message == null){
-            Toast.makeText(this, "Tag is empty", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        return true;
+        super.onNewIntent(intent);
     }
 
+    /**
+     * Order of Ndef goes:
+     * Tag --contains--> NdefMessages --contain--> NdefRecords
+     * and and NdefRecord is of the format:
+     * [Type Name Format], [Record Type Definition], [Payload Length], [Payload]
+     *
+     * We want to write text, therefore we use TNF_WELL_KNOWN, and RTD_TEXT
+     * @param text - The text to be written
+     * @param tag - The tag onto which writing needs to be done
+     */
+    private void writeTag(String text, Tag tag){
+        NdefRecord record = createRecord(text);
+        NdefRecord[] records = new NdefRecord[]{record};
+        NdefMessage message = new NdefMessage(records);
 
-    private boolean NFCHardwareAvailable(){
-        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if(nfcAdapter == null){
-            DialogFragment noNfc = new NFCInfoDialog();
-            Bundle noNFCBundle = new Bundle();
-            noNFCBundle.putString("title", "NFC Error");
-            noNFCBundle.putString("message", "NFC not supported on this device");
-            noNFCBundle.putString("type", "noNFC");
-            noNfc.setArguments(noNFCBundle);
-            noNfc.show(getFragmentManager(), "NFC OFF");
-            return false;
+        // Check the tag is in Ndef format
+        Ndef ndef = Ndef.get(tag);
+        if(ndef != null){
+            try {
+                ndef.connect(); // establish connection
+                ndef.writeNdefMessage(message);
+                Toast.makeText(NFC_Tag_Writer.this, "Ndef Write successful", Toast.LENGTH_SHORT).show();
+                ndef.close(); //close connection
+            } catch (IOException e) {
+                Toast.makeText(NFC_Tag_Writer.this, "Failed to write, tag may have moved", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            } catch (FormatException e) {
+                Toast.makeText(NFC_Tag_Writer.this, "Tag is of invalid format", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        } else {
+            // If it's not in Ndef Format, format it if possible
+            NdefFormatable formattable = NdefFormatable.get(tag);
+            if(formattable != null){
+                try {
+                    formattable.connect();
+                    formattable.format(message);
+                    Toast.makeText(NFC_Tag_Writer.this, "Ndef Format and write successful", Toast.LENGTH_SHORT).show();
+                    formattable.close();
+                }  catch (IOException e) {
+                    Toast.makeText(NFC_Tag_Writer.this, "Failed to write, tag may have moved", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                } catch (FormatException e) {
+                    Toast.makeText(NFC_Tag_Writer.this, "Tag is of invalid format", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(NFC_Tag_Writer.this, "Invalid Tag format", Toast.LENGTH_SHORT).show();
+            }
         }
-        if(!nfcAdapter.isEnabled()){
-            DialogFragment nfcOff = new NFCInfoDialog();
-            Bundle NFCoffBundle = new Bundle();
-            NFCoffBundle.putString("title", "NFC Error");
-            NFCoffBundle.putString("message", "Please turn on NFC");
-            NFCoffBundle.putString("type", "nfcOff");
-            nfcOff.setArguments(NFCoffBundle);
-            nfcOff.show(getFragmentManager(), "NFC OFF");
-            return false;
-        }
-        return true;
+    }
+    private NdefRecord createRecord(String text){
+        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], text.getBytes());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.back_button_menu, menu);
         return true;
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent){
-        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
-            nfcTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            Toast.makeText(this, "Tag Detected" + nfcTag.toString(), Toast.LENGTH_LONG ).show();
-        }
-    }
-
-    private void readTag(Intent intent){
-        Ndef ndef = Ndef.get(nfcTag);
-        try{
-            ndef.connect();
-
-            NdefMessage message = ndef.getNdefMessage();
-            NdefRecord[] records = message.getRecords();
-
-            String allText = "";
-            for(NdefRecord record : records) {
-                byte[] payload = record.getPayload();
-                String text = new String(payload);
-                allText += text;
-            }
-            tv.setText(allText);
-
-            ndef.close();
-
-            } catch (IOException e) {
-                Toast.makeText(this, "Unable to read NFC tag, try again.", Toast.LENGTH_SHORT).show();
-
-            } catch (FormatException e) {
-                e.printStackTrace();
-            }
     }
 
     @Override
